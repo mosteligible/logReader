249d5e4c54ef0d61ac608c12fe358ec1102c217a
mosteligible <mosteligible@mosteligible>
2023-01-29 15:46:25 -0500

    test updated

--- Client/tests/test_db.py
+++ Client/tests/test_db.py
@@ -6,12 +6,12 @@ from tests.conftest import TEST_DB, TEST_DB_TABLE_NAME
 
 class TestDB(TestCase):
     def testDatabaseOperation(self):
-        payloads = (
+        payloads = [
             {"id": "anid1", "name": "compname1", "plan": "small", "token": "tok00001"},
             {"id": "anid2", "name": "compname2", "plan": "medium", "token": "tok00002"},
             {"id": "anid3", "name": "compname3", "plan": "large", "token": "tok00003"},
             {"id": "anid4", "name": "compname4", "plan": "medium", "token": "tok00004"},
-        )
+        ]
         for load in payloads:
             self.assertEqual(TEST_DB.AddEntry(load, tableName=TEST_DB_TABLE_NAME), True)
 
@@ -22,7 +22,7 @@ class TestDB(TestCase):
             self.assertDictEqual(clientInfo, payloads[index])
 
         obtainedClients = TEST_DB.RetreiveAllClients(tableName=TEST_DB_TABLE_NAME)
-        self.assertTupleEqual(obtainedClients, payloads)
+        self.assertListEqual(obtainedClients, payloads)
 
         for id in clientIds:
             result = TEST_DB.DeleteClient(id, tableName=TEST_DB_TABLE_NAME)
